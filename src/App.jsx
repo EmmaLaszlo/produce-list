e64905f9d864c4c733c81d9c2fb652592e8b4cfe
Emma Grace <emmalaslo@gmail.com>
2025-03-25 16:08:01 -0400

    produce list finalized

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,26 @@ import Total from "./components/Total";
 
 function App() {
   const produceItems = [
-    { name: "Apples", price: 1.2 },
-    { name: "Bananas", price: 0.8 },
-    { name: "Carrots", price: 0.5 },
-    { name: "Tomatoes", price: 1.5 }
+    { name: "Zucchini", price: 2.99 },
+    { name: "Bananas", price: 0.5 },
+    { name: "Red Bell Pepper", price: 2.69 },
+    { name: "Broccoli", price: 2.99 },
+    { name: "Blueberries", price: 4.49},
+    { name: "Lime", price: 0.5},
+    { name: "Strawberries", price: 3.59},
+    { name: "Tomatoes", price: 2.24},
+    { name: "Onion", price: 1.69},
+    { name: "Avacado", price: 1.49},
+    { name: "Fennel", price: 2.49},
+    { name: "Mango", price: 1.99},
+    { name: "Corn", price: 1.99},
+    { name: "Apple", price: 1.52},
+    { name: "Lemon", price: 0.89},
+    { name: "Asparagus", price: 3.49},
+    { name: "Ginger", price: 3.99},
+    { name: "Garlic", price: 3.99},
+    { name: "Potato", price: 0.99},
+    { name: "Kiwi", price: 0.99},
   ];
 
   const [cart, setCart] = useState({});
